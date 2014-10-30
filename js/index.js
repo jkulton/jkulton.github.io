fe8aa444a3ab55d1c54b2b7522c30174d0cfbaf8
Jonathan Kulton <jonkulton@gmail.com>
2014-10-29 23:55:48 -0400

    No.

---
+++ js/index.js
@@ -0,0 +1,13 @@
+$(function() {
+
+    var randomColor = Math.floor(Math.random()*16777215).toString(16);
+
+    $("body").css({
+    
+        backgroundColor: '#' + randomColor
+    
+    });
+    
+    $("#colorcode").text("#" + randomColor);
+
+});
