388d0656582781865e32687a50deacef2282a51e
Jon Kulton <jonkulton@gmail.com>
2022-05-02 21:52:09 -0400

    refresh tailwindcss and add post about httpstatuses

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,9 @@
+const cssnano = require('cssnano')({ preset: 'default' })
+
 module.exports = {
   plugins: [
-    require('postcss-import'),
-    require('tailwindcss')('./tailwind.config.js'),
     require('autoprefixer'),
+    require('tailwindcss')('./tailwind.config.js'),
+    ...(process.env.NODE_ENV === 'production' ? [cssnano] : [])
   ]
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,18 @@ module.exports = {
   purge: [
     './_includes/*.html',
     './_layouts/*.html',
+    './_posts/*.html',
     './_posts/*.md',
     './*.html',
     './*.md',
   ],
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
   plugins: [
-    require('@tailwindcss/typography'),
-  ],
+    require('@tailwindcss/typography')
+  ]
 }
